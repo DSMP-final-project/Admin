d099e32ea3fd38be418245d65fb9ec1a4bf10884
Janith Ranasinghe <janithraditha@gmail.com>
2025-03-09 11:38:25 +0530

    basic routing done

---
+++ src/app/components/setting/setting.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-setting',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      setting works!
+    </p>
+  `,
+  styleUrl: './setting.component.css'
+})
+export class SettingComponent {
+
+}
--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -20,28 +20,43 @@ import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
     RouterOutlet,
   ],
   template: `
-    <mat-toolbar color="primary">
-      <button mat-icon-button (click)="sidenav.toggle()">
-        <mat-icon>menu</mat-icon>
-      </button>
-      <span>SecretSanta Admin Panel</span>
-      <span class="toolbar-spacer"></span>
+    <mat-toolbar color="primary" style="display: flex; justify-content: space-between">
+      <div style="display: flex; align-items: center">
+        <button mat-icon-button (click)="sidenav.toggle()">
+          <mat-icon>menu</mat-icon>
+        </button>
+        <span>SecretSanta Admin Panel</span>
+        <span class="toolbar-spacer"></span>
+      </div>
+      <div>
+        <button mat-icon-button>
+          <mat-icon>logout</mat-icon>
+        </button>
+      </div>
     </mat-toolbar>
 
     <mat-sidenav-container class="sidenav-container">
       <mat-sidenav #sidenav mode="side" class="sidenav">
         <mat-nav-list>
-          <a mat-list-item routerLink="/customer" routerLinkActive="active" ariaCurrentWhenActive="page">
+          <a mat-list-item routerLink="/dashboard/stats" ariaCurrentWhenActive="page">
+            <mat-icon matListItemIcon>bar_chart</mat-icon>
+            <span matListItemTitle>Stats</span>
+          </a>
+          <a mat-list-item routerLink="/dashboard/customer" ariaCurrentWhenActive="page">
             <mat-icon matListItemIcon>person</mat-icon>
             <span matListItemTitle>Customer</span>
           </a>
-          <a mat-list-item routerLink="/order" routerLinkActive="active" ariaCurrentWhenActive="page">
+          <a mat-list-item routerLink="/dashboard/order" ariaCurrentWhenActive="page">
             <mat-icon matListItemIcon>shopping_cart</mat-icon>
             <span matListItemTitle>Order</span>
           </a>
-          <a mat-list-item routerLink="/product" routerLinkActive="active" ariaCurrentWhenActive="page">
+          <a mat-list-item routerLink="/dashboard/product" ariaCurrentWhenActive="page">
             <mat-icon matListItemIcon>inventory_2</mat-icon>
             <span matListItemTitle>Product</span>
+          </a>
+          <a mat-list-item routerLink="/dashboard/setting" ariaCurrentWhenActive="page">
+            <mat-icon matListItemIcon>settings</mat-icon>
+            <span matListItemTitle>Setting</span>
           </a>
         </mat-nav-list>
       </mat-sidenav>
